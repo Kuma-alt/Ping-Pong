f63aea62a8f78fb5ad1c8852212cff60e22b9b5d
Vitaliy <55349633+Kuma-alt@users.noreply.github.com>
2023-05-14 17:59:10 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -1,8 +1,10 @@
 from pygame import *
-from random import randint
-from time import time as timer
+#from random import randint
+#from time import time as timer
 
 back = (71, 131, 160) 
+win_width = 600
+win_height = 500
 window = display.set_mode((600, 500))
 display.set_caption("Пінг-Понг")
 window.fill(back)
@@ -23,9 +25,9 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 400:
+        if keys[K_s] and self.rect.y < 400:
             self.rect.y += self.speed 
             
     def update_r(self):
@@ -35,33 +37,22 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < 400:
             self.rect.y += self.speed 
 
-racket1 = Player('racket.png', 30, 200, 4, 50, 150)
-racket2 = Player('racket.png', 30, 200, 4, 50, 150)
-ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
-            
-''''            
-ship = Player("rocket.png", 5, 400, 10)
-
-monsters = sprite.Group()
-for i in range(1, 4):
-    monster = Enemy("ufo.png", randint(80, win_width - 80), -40, randint(1, 3))
-    monsters.add(monster)
-
-bullets = sprite.Group()
-
-asteroids = sprite.Group()
-for i in range(1, 3):
-    asteroid = Asteroid("asteroid.png", randint(30, win_width - 30), -40, randint(1, 6))
-    asteroids.add(asteroid)
-
-window = display.set_mode((win_width, win_height))
-display.set_caption("Шутер")
-background = transform.scale(image.load("galaxy.jpg"), (win_width, win_height))
-''''
+racket1 = Player('racket.jpg', 30, 200, 4, 50, 150)
+racket2 = Player('racket.jpg', 30, 200, 4, 50, 150)
+ball = GameSprite('ball.jpg', 200, 200, 4, 50, 50)
 
 finish = False
 game = True
+clock = time.Clock()
+FPS = 60
 
+font.init()
+font = font.font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
 
 while game:
     for e in event.get():
@@ -69,10 +60,36 @@ while game:
             game = False
 
      if finish != True:
+        window.fill(back)
         racket1.update_l()
         racket2.update_r()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
+        
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+            
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= 1
+            
+        if ball.rect.x < 0
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+            
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+            
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+        
+    display.update()
+    clock.tick(FPS)
+        
 '''''
     if not finish:
         window.blit(background, (0, 0))
